e161d8d162445ecf32943344eb3e4631d4f6d068
vishnubhandarge <vishnubhandarge@icloud.com>
2024-08-13 10:48:43 +0530

    First Commit

---
+++ src/app/components/data-binding/data-binding.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+
+}
